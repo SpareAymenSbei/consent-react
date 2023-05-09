2470fa74989a7e3079a7c8eb2b9f10c4692c106e
SpareAymenSbei <masbei@tryspare.com>
2023-05-09 12:13:38 +0100

    updated header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,11 +1,14 @@
 import classes from './Header.module.scss';
 import logo from '../../assets/logo.png'
+import  {BiArrowBack} from 'react-icons/bi'
 function Header() {
   return (
     <div className={classes.header}>
+     <BiArrowBack className={classes.icon}/>
       <div className={classes.imgHolder}>
         <img src={logo} alt="logo" />
       </div>
+      <div></div>
     </div>
   )
 }
